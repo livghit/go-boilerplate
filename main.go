9588cb6a1d5fdf10392a8602f7e947e7164b6c87
Liv Ghit <livghit@Livs-MacBook-Pro.local>
2023-06-13 14:10:26 +0200

    Small project fixes

--- main.go
+++ main.go
@@ -1,21 +1,6 @@
 package main
 
-import "github.com/livghit/go-boilerplate/app/router"
-import "github.com/gin-gonic/gin"
 
 func main() {
-	r := router.NewRouter()
-
-	r.AddRoute("GET", "/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "ping",
-		})
-	})
-	r.AddRoute("GET", "/pong", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.RouterServe()
+  // Main Func to run the server 
 }
