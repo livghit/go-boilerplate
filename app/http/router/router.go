d5c03330771e521bd30f3048110511ced973ce83
Liv Ghit <livghit@Livs-MBP.fritz.box>
2023-06-17 14:29:48 +0200

    Soooo I am doing some stuff

---
+++ app/http/router/router.go
@@ -0,0 +1,9 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// Router and Routes setup
+func SetupRouter(app *fiber.App) {
+}
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"github.com/livghit/go-boilerplate/routes"
+	"github.com/livghit/go-boilerplate/app/http/router"
 )
 
 func main() {
 	// Main Func to run the server
-
-	routes.PrintRoutes()
-	routes.LoadRoutes()
-	routes.PrintRoutes()
 }
